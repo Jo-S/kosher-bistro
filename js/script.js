e28ab544232c5b05f7f614c34adaef9ba67d91bb
jsjackson263 <jsjackson263@users.noreply.github.com>
2019-01-12 21:39:43 +1300

    Dynamically Loading Home View Content

--- js/script.js
+++ js/script.js
@@ -18,5 +18,41 @@ $(function() {//same as document.addEventListener("DOMContentLoaded") ......
   //   $(event.target).focus();
   // });
 
-
 });
+
+(function(global) {
+
+  var dc = {};
+
+  var homeHtml = "snippets/home-snippet.html";
+
+  // Convenience function for inserting innerHTML for 'select'
+  var insertHtml = function(selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+  // Show loading icon inside element identified by 'selector'
+  var showLoading = function(selector) {
+    var html = "<div class='text-center'>";
+    html += "<img src='images/ajax-loader.gif'></div>"; //see www.ajaxload.info
+    insertHtml(selector, html);
+  };
+
+  // On page load (before images or CSS)
+  document.addEventListener("DOMContentLoaded", function(event) {
+
+    // On firsr load, show home view
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+      homeHtml,
+      function(responseText){
+      document.querySelector("#main-content").innerHTML = responseText;
+      },
+      false);
+
+  });
+
+global.$dc = dc;
+
+})(window);
