1904d7306eee2cbc9728937f488e19486c7dd4c7
jsjackson263 <jsjackson263@users.noreply.github.com>
2019-01-12 22:53:20 +1300

    Use secure XMLHttpRequest endpoints in order to allow deployment on GitHub

--- js/script.js
+++ js/script.js
@@ -26,11 +26,11 @@ $(function() {//same as document.addEventListener("DOMContentLoaded") ......
 
   var homeHtml = "snippets/home-snippet.html";
 
-  var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+  var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
   var categoriesTitleHtml = "snippets/categories-title-snippet.html";
   var categoryHtml = "snippets/category-snippet.html";
 
-  var menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
   var menuItmesTitleHtml = "snippets/menu-items-title.html";
   var menuItemHtml = "snippets/menu-item.html";
 
